d7f2f7d36d4637f4c493fd53c150c7ca94a25072
Taylor Moore <Tmoore2272@gmail.com>
2015-11-23 14:00:18 -0700

    I3, complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,7 +1,28 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+Number = rand(100)
+def guesses(guess)
+  gus = guess.to_i
+  value = Number - gus
+  if value.between?(1,5)
+    "#{gus} is close, but to low!"
+  elsif value.between?(-5,-1)
+    "#{gus} is close, but to high!"
+  elsif Number < gus
+    "#{gus} is way to high!!"
+  elsif Number > gus
+    "#{gus} is way to low!!"
+  else
+    "YOU ROCK!"
+  end
+end
+
+
 get '/' do
-  erb :index, :locals => {:number => number}
+  # throw params.inspect
+  guess = params["guess"]
+  message = guesses(guess)
+  erb :index, :locals => {:number => Number, :message => message}
+
 end
